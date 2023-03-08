573fe9f18639ed0d35784f4ccf4027ac62c9dae5
Prachi Mahableshwarkar <prachimahableshwarkar@Prachis-MacBook-Pro.local>
2023-03-08 12:04:23 -0500

    abs resid

--- joint_analyses/cues/LME_cueAnalysis.R
+++ joint_analyses/cues/LME_cueAnalysis.R
@@ -18,88 +18,93 @@ df <- read.csv('data_for_lme.csv')
 
 # Create models 
 # fixed effect only:
-m_dur<- lm(residuals ~ duration, data=df)
+m_dur<- lm(abs_residuals ~ duration, data=df)
 summary(m_dur)
 
-m_stim<- lm(residuals ~ stimulus, data=df)
+m_stim<- lm(abs_residuals ~ stimulus, data=df)
 summary(m_stim)
 
 # m0
 # fixed effect: clutter
 # random effect: duration
-m0 <- lmer(residuals ~ clutter + (1|duration), data=df)
+m0 <- lmer(abs_residuals ~ clutter + (1|duration), data=df)
 summary(m0)
 
 
 # m1
 # fixed effect: clutter
 # random effect: stimulus
-m1 <- lmer(residuals ~ clutter + (1|stimulus), data=df)
+m1 <- lmer(abs_residuals ~ clutter + (1|stimulus), data=df)
 summary(m1)
 
 # m2
 # fixed effect: clutter
 # random effect: duration, stimulus
-m2 <- lmer(residuals ~ clutter + (1|duration) + (1|stimulus), data=df)
+m2 <- lmer(abs_residuals ~ clutter + (1|duration) + (1|stimulus), data=df)
 summary(m2)
 
 #m3
 # fixed effect: groundPlane
 # random effect: duration
-m3 <- lmer(residuals ~ groundPlane + (1|duration), data=df)
+m3 <- lmer(abs_residuals ~ groundPlane + (1|duration), data=df)
 summary(m3)
 
 #m4
 # fixed effect: groundPlane
 # random effect: stimulus
-m4 <- lmer(residuals ~ groundPlane + (1|stimulus), data=df)
+m4 <- lmer(abs_residuals ~ groundPlane + (1|stimulus), data=df)
 summary(m4)
 
 #m5
 # fixed effect: groundPlane
 # random effect: stimulus, duration
-m5 <- lmer(residuals ~ groundPlane + (1|stimulus)+ (1|duration), data=df)
+m5 <- lmer(abs_residuals ~ groundPlane + (1|stimulus)+ (1|duration), data=df)
 summary(m5)
 
 #m6
 # fixed effects: groundPlane & clutter
 # random effect: duration
-m6 <- lmer(residuals ~ groundPlane + clutter + (1|duration), data=df)
+m6 <- lmer(abs_residuals ~ groundPlane + clutter + (1|duration), data=df)
 summary(m6)
 
 #m7
 # fixed effects: groundPlane & clutter
 # random effect: stimulus
-m7 <- lmer(residuals ~ groundPlane + clutter + (1|stimulus), data=df)
+m7 <- lmer(abs_residuals ~ groundPlane + clutter + (1|stimulus), data=df)
 summary(m7)
 
 #m8
 # fixed effects: groundPlane & clutter
 # random effect: duration, stimulus
-m8 <- lmer(residuals ~ groundPlane + clutter + (1|stimulus) + (1|duration), data=df)
+m8 <- lmer(abs_residuals ~ groundPlane + clutter + (1|stimulus) + (1|duration), data=df)
 summary(m8)
 
 #m9
 # fixed effects: groundPlane, clutter
 # random effect: duration
 #interaction: groundPlane * clutter
-m9 <- lmer(residuals ~ groundPlane + clutter + groundPlane*clutter + (1|duration), data=df)
+m9 <- lmer(abs_residuals ~ groundPlane + clutter + groundPlane*clutter + (1|duration), data=df)
 summary(m9)
 
 #m10
 # fixed effects: groundPlane, clutter
 # random effect: stimulus
 #interaction: groundPlane * clutter
-m10 <- lmer(residuals ~ groundPlane + clutter + groundPlane*clutter + (1|stimulus), data=df)
+m10 <- lmer(abs_residuals ~ groundPlane + clutter + groundPlane*clutter + (1|stimulus), data=df)
 summary(m10)
 
 #m11
 # fixed effects: groundPlane, clutter
 # random effect: duration, stimulus
 #interaction: groundPlane * clutter
-m11 <- lmer(residuals ~ groundPlane + clutter + groundPlane*clutter + (1|stimulus) + (1|duration), data=df)
+m11 <- lmer(abs_residuals ~ groundPlane + clutter + groundPlane*clutter + (1|stimulus) + (1|duration), data=df)
 summary(m11)
 
+# if you do the interaction, it will automatically give the main effects too
+# stim as random effect, subj as random effect, abs(residuals)
+# figure out how to get plots of these estimates 
+m12 <- lmer(abs_residuals ~ duration*clutter*groundPlane + (1|stimulus), data=df)
+summary(m12)
 
 # model comparison
 anova(m0,m1,m2,m3,m4,m5,m6,m7,m8,m9,m10,m11)
