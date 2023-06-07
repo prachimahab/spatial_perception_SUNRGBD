ff77eb5e58a8f088740b483d61c0e8b361054887
Prachi Mahableshwarkar <prachimahableshwarkar@Prachis-MacBook-Pro.local>
2023-06-07 01:15:58 -0400

    figs

---
+++ joint_analyses/cues/LME_code/LME_subjResidual_groundPlane_durations.R
@@ -0,0 +1,110 @@
+# load LME package
+library('lme4')
+library(nlme)
+library('lmerTest')
+library('partR2') # https://cran.r-project.org/web/packages/partR2/vignettes/Using_partR2.html
+library(AICcmodavg)
+library(MuMIn)
+library(multilevel) 
+library('r2glmm')
+
+library(tidyverse) #for all data wrangling
+library(cowplot) #for manuscript ready figures
+library(sjPlot) #for plotting lmer and glmer mods
+library(sjmisc) 
+library(effects)
+library(sjstats) #use for r2 functions
+
+# Load data
+setwd("/Users/prachimahableshwarkar/Documents/GW/Depth_MTurk/spatial_perception_SUNRGBD/joint_analyses/cues/")
+df_125 <- read.csv('125_groundPlane_participantData_for_lme.csv')
+df_125$subjID <- factor(df_125$subjID) 
+df_125$duration <- factor(df_125$duration)
+df_125$stimulus <- factor(df_125$stimulus) 
+df_grouped_125 <- read.csv('125_groundPlane_participantData_grouped_for_lme.csv')
+
+mod_main_abs_125 <- lmer(abs_s_residual ~ groundPlane + (1|actual_depth), data=df_125)
+summary(mod_main_abs_125)
+
+df_250 <- read.csv('250_groundPlane_participantData_for_lme.csv')
+df_250$subjID <- factor(df_250$subjID) 
+df_250$duration <- factor(df_250$duration)
+df_250$stimulus <- factor(df_250$stimulus) 
+df_grouped_250 <- read.csv('250_groundPlane_participantData_grouped_for_lme.csv')
+
+mod_main_abs_250 <- lmer(abs_s_residual ~ groundPlane + (1|actual_depth), data=df_250)
+summary(mod_main_abs_250)
+
+df_1000 <- read.csv('1000_groundPlane_participantData_for_lme.csv')
+df_1000$subjID <- factor(df_1000$subjID) 
+df_1000$duration <- factor(df_1000$duration)
+df_1000$stimulus <- factor(df_1000$stimulus) 
+df_grouped_1000 <- read.csv('1000_groundPlane_participantData_grouped_for_lme.csv')
+
+mod_main_abs_1000 <- lmer(abs_s_residual ~ groundPlane + (1|actual_depth), data=df_1000)
+summary(mod_main_abs_1000)
+
+# sjPlot::plot_model(mod_main,
+#                    show.values=TRUE, show.p=TRUE, 
+#                    title="")
+# sjPlot:: tab_model(mod_main)
+# 
+# sjPlot::plot_model(mod_main_abs,
+#                    show.values=TRUE, show.p=TRUE, 
+#                    title="")
+# sjPlot:: tab_model(mod_main_abs)
+
+# Plotting code: https://lmudge13.github.io/sample_code/mixed_effects.html
+
+gp_effects_abs_125 <- effects::effect(term= "groundPlane", mod= mod_main_abs_125)
+summary(gp_effects_abs_125) #output of what the values are
+
+# Save the effects values as a df:
+gp_effects_abs_df_125 <- as.data.frame(gp_effects_abs_125)
+
+gp_effects_abs_250 <- effects::effect(term= "groundPlane", mod= mod_main_abs_250)
+summary(gp_effects_abs_250) #output of what the values are
+
+# Save the effects values as a df:
+gp_effects_abs_df_250 <- as.data.frame(gp_effects_abs_250)
+
+gp_effects_abs_1000 <- effects::effect(term= "groundPlane", mod= mod_main_abs_1000)
+summary(gp_effects_abs_1000) #output of what the values are
+
+# Save the effects values as a df:
+gp_effects_abs_df_1000 <- as.data.frame(gp_effects_abs_1000)
+
+#1
+#1
+groundPlane_plot <- ggplot() + 
+  theme_minimal() +  # Removes background and grid
+  theme(
+    panel.grid = element_blank(),  # Removes grid lines
+    panel.background = element_blank(),  # Removes background
+    axis.text = element_text(size = 15),  # Increase axis tick label font size
+    axis.title = element_text(size = 17),  # Increase axis label font size
+    axis.line = element_line(color = "black"),  # Sets the color of axis spines
+    # Add tick marks
+    axis.ticks = element_line(color = "black"),
+    panel.border = element_blank()  # Remove panel border
+  ) +
+  
+  # geom_point(data = gp_effects_abs_df_125, aes(x = groundPlane, y = fit), color = "darkgreen") +
+  geom_line(data = gp_effects_abs_df_125, aes(x = groundPlane, y = fit), color = "darkgreen") +
+  geom_ribbon(data = gp_effects_abs_df_125, aes(x = groundPlane, ymin = lower, ymax = upper), alpha = 0.2, fill = "darkgreen") +
+
+  # geom_point(data = gp_effects_abs_df_1000, aes(x = groundPlane, y = fit), color = "darkviolet") +
+  geom_line(data = gp_effects_abs_df_1000, aes(x = groundPlane, y = fit), color = "darkviolet") +
+  geom_ribbon(data = gp_effects_abs_df_1000, aes(x = groundPlane, ymin = lower, ymax = upper), alpha = 0.2, fill = "darkviolet") +
+
+  # geom_point(data = gp_effects_abs_df_250, aes(x = groundPlane, y = fit), color = "chocolate") +
+  geom_line(data = gp_effects_abs_df_250, aes(x = groundPlane, y = fit), color = "chocolate") +
+  geom_ribbon(data = gp_effects_abs_df_250, aes(x = groundPlane, ymin = lower, ymax = upper), alpha = 0.2, fill = "chocolate") +
+  #6
+  labs(x = "Complete Ground Plane", y = "Abs(Residuals)") +
+  #7
+  scale_y_continuous(breaks=c(.4, 0.5, .6, 0.7, .8, 0.9), limits = c(0.4, 0.95))
+
+# Display the plot
+print(groundPlane_plot)
+
